bfe3eb81d69887c347548d52b2107a0df67bfb94
Kousha Najafi <kousha@lazyhack.com>
2014-12-08 18:51:08 -0800

    Modify entry point so that it can be easily overridden with the docker run command.
    To configure Kafka's advertised hostname, you override the HOSTNAME
    environment variable for the container.

--- kafka/kafka-config-start.sh
+++ kafka/kafka-config-start.sh
@@ -5,11 +5,11 @@ function hostname_hash() {
   echo $((0x${hash%% *}))
 }
 
-hostname="${1:-localhost}"
+hostname="${HOSTNAME:-localhost}"
 cd "${KAFKA_HOME}"
 
 sed -i "s/#advertised.host.name.*/advertised.host.name=${hostname}/" "${KAFKA_HOME}/config/server.properties"
 
 sed -i "s/broker.id=0/broker.id=$(hostname_hash ${hostname})/" "${KAFKA_HOME}/config/server.properties"
 
-exec bin/kafka-server-start.sh config/server.properties
+exec "$@"
