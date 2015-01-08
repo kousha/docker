79cd22e64cd668b6ca46e024264e20d0ede55489
Kousha Najafi <kousha@lazyhack.com>
2015-01-08 13:49:23 -0800

    Enabling automatic leader rebalancing.

--- kafka/kafka-config-start.sh
+++ kafka/kafka-config-start.sh
@@ -6,10 +6,13 @@ function hostname_hash() {
 }
 
 hostname="${HOSTNAME:-localhost}"
+server_properties="${KAFKA_HOME}/config/server.properties"
 cd "${KAFKA_HOME}"
 
-sed -i "s/#advertised.host.name.*/advertised.host.name=${hostname}/" "${KAFKA_HOME}/config/server.properties"
+sed -i "s/#advertised.host.name.*/advertised.host.name=${hostname}/" "${server_properties}"
 
-sed -i "s/broker.id=0/broker.id=$(hostname_hash ${hostname})/" "${KAFKA_HOME}/config/server.properties"
+sed -i "s/broker.id=0/broker.id=$(hostname_hash ${hostname})/" "${server_properties}"
+
+echo "auto.leader.rebalance.enable=true" >> "${server_properties}"
 
 exec "$@"
