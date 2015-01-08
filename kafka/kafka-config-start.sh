cf19caeca8705155abac9f00b6ad46caa8a0f33e
Kousha Najafi <kousha@lazyhack.com>
2015-01-08 14:42:07 -0800

    Make only append to config once.

--- kafka/kafka-config-start.sh
+++ kafka/kafka-config-start.sh
@@ -13,6 +13,8 @@ sed -i "s/#advertised.host.name.*/advertised.host.name=${hostname}/" "${server_p
 
 sed -i "s/broker.id=0/broker.id=$(hostname_hash ${hostname})/" "${server_properties}"
 
-echo "auto.leader.rebalance.enable=true" >> "${server_properties}"
+if ! grep -q "auto.leader.rebalance.enable=true" "${server_properties}"; then
+  echo "auto.leader.rebalance.enable=true" >> "${server_properties}"
+fi
 
 exec "$@"
